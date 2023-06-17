c4faeaa72668db3e33c80e85412caf5a00c3e16e
Andrew Uzhva <65554379+krupchynskyiai@users.noreply.github.com>
2023-06-17 22:59:28 +0300

    replace loader

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
-import { Loader } from "./components/Loader/Loader";
 import { Box } from "components/Box";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Симулюємо завантаження даних або інших асинхронних операцій
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
   return (
     <Box>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Box>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,38 @@
+import React, { useState, useEffect } from "react";
+
 import { Box } from "components/Box";
 import { Header } from "components/Header/Header";
 import { RequestBetaBtn } from "components/RequestBetaBtn";
 import Hero from "components/MainLayout/HomeScreens/Hero/Hero";
 import Cases from "components/MainLayout/HomeScreens/Cases/Cases";
 
+import { Loader } from "components/Loader/Loader";
+
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Симулюємо завантаження даних або інших асинхронних операцій
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <Box position={"relative"}>
-      <Header />
-      <Hero />
-      <Cases />
-      <Box position={"fixed"} top={"calc(100vh - 260px)"} right={28}>
-        <RequestBetaBtn size={228} />
-      </Box>
-    </Box>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Box position={"relative"}>
+          <Header />
+          <Hero />
+          <Cases />
+          <Box position={"fixed"} top={"calc(100vh - 260px)"} right={28}>
+            <RequestBetaBtn size={228} />
+          </Box>
+        </Box>
+      )}
+    </>
   );
 };
 
