60ccfa9175d923546121d96448c14632d70be7c9
Andrew Uzhva <65554379+krupchynskyiai@users.noreply.github.com>
2023-06-17 23:25:45 +0300

    add base name and change logo href

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import logoHeader from "assets/svg/logoHeader.svg";
 export const Logo = () => {
   return (
     <SC.LogoContainer as={"div"}>
-      <SC.LogoLink href="/">
+      <SC.LogoLink href="/animation-landing">
         <SC.LogoImage src={logoHeader} alt="Logo" />
       </SC.LogoLink>
     </SC.LogoContainer>
