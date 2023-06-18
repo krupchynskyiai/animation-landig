beebf70935332dc363a08c59567ff7add3353535
Andrew Uzhva <65554379+krupchynskyiai@users.noreply.github.com>
2023-06-18 22:21:14 +0300

    fix FAQ's padding

--- src/components/MainLayout/HomeScreens/Faqs/Faqs.styled.js
+++ src/components/MainLayout/HomeScreens/Faqs/Faqs.styled.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const FaqsSection = styled.section`
+  display: flex;
   min-height: 100vh;
   padding-top: 93px;
 `;
 
-export const FaqsContainer = styled.div``;
+export const FaqsContainer = styled.div`
+  margin-top: auto;
+`;
